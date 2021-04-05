0c8a909ae180d1673b70dafb826fedd8b5bf326c
MrMaguila123 <gabrielgf6000@gmail.com>
2021-04-05 20:50:47 -0300

    teste 01

---
+++ project/src/project/Main.java
@@ -0,0 +1,22 @@
+package project;
+
+
+
+
+public class Main {
+	
+	
+
+	public static void main(String[] args)
+	{
+		Usuario a = new Usuario();
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		
+		System.out.println("Enter a line: ");
+		sc.nextLine();
+		sc.close();
+		return;
+	}
+	
+}
+
